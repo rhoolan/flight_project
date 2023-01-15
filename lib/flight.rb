a6602e12eca2a1e0e0babcb7e171af1c56c847d7
Rob Hoolan <rhoolan@gmail.com>
2023-01-16 00:02:30 +0900

    add list_passengers function

--- lib/flight.rb
+++ lib/flight.rb
@@ -14,4 +14,8 @@ attr_reader :passengers
     def board_passenger(passenger)
         @passengers << passenger if passenger.has_flight?(@flight_number) && !full?
     end
+
+    def list_passengers
+        @passengers.map { |passenger| passenger.name }
+    end
 end
