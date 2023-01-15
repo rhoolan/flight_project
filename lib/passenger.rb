ec341bb0d98bca0ff8c06991d2ae2601c272354d
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 23:44:13 +0900

    add add_flight function

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -9,4 +9,9 @@ class Passenger
     def has_flight?(flight_number)
         @flight_numbers.include?(flight_number.upcase)
     end
+
+    def add_flight(flight_number)
+       @flight_numbers << flight_number.upcase if !has_flight?(flight_number.upcase)
+    end
+
 end
