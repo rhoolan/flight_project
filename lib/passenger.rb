963172d4fd53bdbacd2343b7059e8ed7b268c8c3
Rob Hoolan <rhoolan@gmail.com>
2023-01-15 23:39:25 +0900

    add has_flight? function

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -5,4 +5,8 @@ class Passenger
         @name = name
         @flight_numbers = []
     end
+
+    def has_flight?(flight_number)
+        @flight_numbers.include?(flight_number.upcase)
+    end
 end
